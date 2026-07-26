a8e38eb4e9a82bef9a1da0139d2943c549f0b9c6
Kushal A N <kushalan01@gmail.com>
2026-07-26 13:19:16 +0000

    Memory usage

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -9,27 +9,47 @@ const isLoggedIn = false;
 const outsideTemp = null;
 let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+const id = Symbol("123");
+const anotherId = Symbol("123");
 
 // console.log(id === anotherId);
 
-const bigNumber = 3456345634536453645364n
-
-
+const bigNumber = 3456345634536453645364n;
 
 // Reference (Non primitive)
 
 // Array, Objects, Functions
 
-const heros = ["Iron Man", "Thor", "Hulk"]
+const heros = ["Iron Man", "Thor", "Hulk"];
 let myObj = {
-    name:"Kushal",
-    age:21,
-}
+  name: "Kushal",
+  age: 21,
+};
 
-const myFunction = function(){
-    // console.log("Hello World");
-}
+const myFunction = function () {
+  // console.log("Hello World");
+};
 
 // console.log(typeof myFunction);
+
+// ----------------------------------------------------------------------
+
+// Stack (Primitive), Heap (Non-primitive)
+
+let myName = "Kushal";
+let anothername = myName;
+anothername = "Ravana";
+
+console.log(myName);
+console.log(anothername);
+
+let userOne = {
+  email: "kushal@gmail.com",
+  upi: "kushal@ybl",
+};
+
+let userTwo = userOne;
+userTwo.email = "ravana@lanka.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
