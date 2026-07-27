c54457c196a74a6f1eea46bf1c6d25fb0ebe5125
Kushal A N <kushalan01@gmail.com>
2026-07-27 12:01:11 +0000

    math and num

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,17 +1,32 @@
-const score = 400
+const score = 400;
 // console.log(score)
 
-const balance = new Number(100)
+const balance = new Number(100);
 // console.log(balance)
 
 // console.log(balance.toString().length)
 // console.log(balance.toFixed(2))
 
-const num = 23.8942
+const num = 23.8942;
 
 // console.log(num.toPrecision(2))
 
-const hundreds = 1000000
+const hundreds = 1000000;
 // console.log(hundreds.toLocaleString('en-IN'))
 
 // ------------------------- Maths -------------------------
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+// console.log(Math.random());
+// console.log((Math.random() * 10) + 1);
+// console.log((Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
