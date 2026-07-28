e96d01f45ef0fd11e8b250f3789bd05cf57cbdc0
Kushal A N <kushalan01@gmail.com>
2026-07-28 12:39:47 +0000

    Intro to functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,22 @@
+function sayMyName() {
+  console.log("K");
+  console.log("U");
+  console.log("S");
+  console.log("H");
+  console.log("A");
+  console.log("L");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(a,b){
+//     console.log(a + b)
+// }
+function addTwoNumbers(a,b){
+    let result = a + b
+    return result
+}
+
+const result = addTwoNumbers(3, 4)
+
+console.log("Result: ",result)
